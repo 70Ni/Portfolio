0bc4b1d6208054a16f329a432e7f0e61dc9f5b28
70ni <4thedevelopments@gmail.com>
2024-06-23 16:20:33 +0530

    added Project section

---
+++ src/Components/ProjectItem/Project.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div>
+        <div className="project-item">
+            <div className="project-list-header">Lead Product Designer</div>
+            <div className="project-paragraph">
+              Lorem maximus dictumst elementum efficitur lobortis per egestas
+              dis tincidunt nam non netus venenatis amet dolor dapibus donec
+              justo augue.
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default Project
