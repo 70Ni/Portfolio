0a82bc46fd148a1a275e2f9d2c7b31a9ddf20164
70ni <4thedevelopments@gmail.com>
2024-09-21 00:08:28 +0530

    Browser router to hashrouter

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import ProjectItem from "./Components/ProjectItem/ProjectItem";
 import Footer from "./Components/Footer/Footer";
 import Navigation from "./Components/Navigation/Navigation";
 import ProView from "./Components/ProVIew/ProView";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Home from "./Components/Home/Home";
 import { ThemeContext } from "./Theme/Theme";
 import { useContext } from "react";
@@ -20,15 +26,15 @@ function App() {
     <div className={`App ${theme}`}>
       <div className="Hero-wrapper">
         <div className="wrp">
-        <BrowserRouter>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/projects" element={<ProView />}></Route>
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </div>
+          <HashRouter>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/projects" element={<ProView />}></Route>
+            </Routes>
+            <Footer />
+          </HashRouter>
+        </div>
       </div>
     </div>
   );
