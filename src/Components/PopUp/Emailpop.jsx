2e801332071a23583c7b8b5c2ab927b8e6733d3b
70ni <4thedevelopments@gmail.com>
2024-09-27 08:14:06 +0530

    pop up items

---
+++ src/Components/PopUp/Emailpop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Emailpop({ isCopy }) {
+  return (
+    <div className="email-popUp">
+      <div className="para emailPop-text">Email Copied!</div>
+    </div>
+  );
+}
+
+export default Emailpop;
