e9516803734edc4d2b91efbe24d209acbc16aed5
70ni <4thedevelopments@gmail.com>
2024-10-07 18:38:58 +0530

    removed word web

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
   return (
     <div className="intro-wrapperhero">
       <div className="intro-wrapper">
-        <div className="header-term Headerfont  ">Front-end web developer</div>
+        <div className="header-term Headerfont  ">Front-end developer</div>
       </div>
       <div className="intro-section">
         <div className="line"></div>
@@ -19,9 +19,10 @@ function Intro() {
           <div className="project-item">
             {/* <div className="project-list-header">Lead Product Designer</div> */}
             <div className="project-paragraph">
-              Lorem maximus dictumst elementum efficitur lobortis per egestas
-              dis tincidunt nam non netus venenatis amet dolor dapibus donec
-              justo augue.
+              I'm a passionate front-end developer at the beginning of my coding
+              journey. Though I don't have formal work experience yet, I have
+              dedicated myself to learning the core technologies and concepts
+              needed to build modern, responsive, and user-friendly websites.
             </div>
           </div>
         </div>
